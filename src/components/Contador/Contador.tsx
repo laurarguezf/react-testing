349c57564c3ba8b5ca25c03411da43a7b11f00f1
Laura Rodríguez <laurarguezfdez@gmail.com>
2024-11-28 20:24:38 +0100

    Refactor Contador.tsx component

--- src/components/Contador/Contador.tsx
+++ src/components/Contador/Contador.tsx
@@ -3,10 +3,14 @@ import { useState } from "react"
 export const Contador = () => {
     const [ contador, setContador ] = useState(0);
 
+    const handleIncrementar = () => {
+        setContador(contador + 1);
+    };
+
     return (
         <div>
             <p>Contador: {contador}</p>
-            <button onClick={() => setContador(contador + 1)}>Incrementar</button>
+            <button onClick={handleIncrementar}>Incrementar</button>
         </div>
     )
 }
