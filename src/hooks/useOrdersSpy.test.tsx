0417b14e5b17b4417ecd180848bab31b2052b1ba
Laura Rodríguez <laurarguezfdez@gmail.com>
2024-12-02 19:15:23 +0100

    Check if error appears using spies and mocks

--- src/hooks/useOrdersSpy.test.tsx
+++ src/hooks/useOrdersSpy.test.tsx
@@ -28,6 +28,17 @@ describe('useOrdersSpy', () => {
     })
 
     test('Debería mostrar un error', async () => {
-        
+        useSessionSpy.mockReturnValue({ user: { id: 1 } });
+        getOrdersSpy.mockRejectedValue(new Error('Api error'));
+        const {result, waitForNextUpdate} = renderHook(() => useOrders());
+
+        expect(result.current.loading).toBe(true);
+        expect(result.current.error).toBeNull();
+
+        await waitForNextUpdate();
+
+        expect(result.current.loading).toBe(false);
+        expect(result.current.error).toBe('Failed to fetch orders. Please try again later.')
+        expect(getOrdersSpy).toHaveBeenCalledTimes(1);
     })
 })
